978d8bde7c4515c8680532da25185893850ac4d1
BIOHAZARD <hazardland@gmail.com>
2020-03-12 19:15:38 +0400

    fix db function

--- config.php
+++ config.php
@@ -1,11 +1,13 @@
 <?php
 
-    function db()
+function db()
+{
+    static $pdo;
+    if ($pdo===null)
     {
-        static $pdo;
-        if ($pdo===null)
-        {
-            $pdo = new \PDO ();
-        }
-        return $pdo;
+        $pdo = new \PDO('mysql:host=127.0.0.1;dbname=test', 'root', '1234');
+        $pdo->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
+        $pdo->setAttribute(PDO::ATTR_EMULATE_PREPARES,false);
     }
+    return $pdo;
+}
